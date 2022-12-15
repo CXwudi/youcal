cfa3b3cd8dc3b872b8b488c22b93335594ed4643
CXwudi <cx2010017@gmail.com>
2022-12-14 23:34:06 -0500

    :monocle_face: better explanation

--- yc-core-ical/src/main/kotlin/mikufan/cx/yc/core/ical/package-info.java
+++ yc-core-ical/src/main/kotlin/mikufan/cx/yc/core/ical/package-info.java
@@ -1,5 +1,7 @@
 /**
- * Contains any lib about creating the ical calendar using the issue lists from YouTrack
+ * Contains any lib about creating the iCal component from an issue component in json form from YouTrack.
+ * <p>
+ * Can be either list of issues to a completed iCal file, or a single issue (component) to a single iCal component.
  * <p>
  * 2022-10-10
  *
