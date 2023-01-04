43e9db293727678282d2821b885dd5c39a1b4139
CXwudi <cx2010017@gmail.com>
2023-01-03 19:29:46 -0500

    Revert "try to enable build scan"
    This reverts commit 09a72a8ed88a23b022a80b7d7b21f5a2c8c6c693.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,12 +17,6 @@ dependencyResolutionManagement {
   // gradle/libs.versions.toml is automatically imported if exists
 }
 
-// from https://stackoverflow.com/a/56634703/8529009
-extensions.findByName("buildScan")?.withGroovyBuilder {
-  setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
-  setProperty("termsOfServiceAgree", "yes")
-}
-
 rootProject.name = "YouCal"
 
 rootDir.listFiles { file -> file.isDirectory && !file.isHidden && file.name.startsWith("yc") }?.forEach {
