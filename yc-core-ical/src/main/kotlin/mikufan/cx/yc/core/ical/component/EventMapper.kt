957f6df7fd28ae0b6275473224ec240a78f1a0d3
CXwudi <cx2010017@gmail.com>
2023-01-03 19:29:19 -0500

    added alarm to EventMapper

--- yc-core-ical/src/main/kotlin/mikufan/cx/yc/core/ical/component/EventMapper.kt
+++ yc-core-ical/src/main/kotlin/mikufan/cx/yc/core/ical/component/EventMapper.kt
@@ -1,8 +1,10 @@
 package mikufan.cx.yc.core.ical.component
 
+import mikufan.cx.inlinelogging.KInlineLogging
 import mikufan.cx.yc.core.ical.model.ToBeMappedYouTrackIssueInfo
 import mikufan.cx.yc.core.ical.util.YouTrackIssueJson
 import mikufan.cx.yc.core.ical.util.addCommonProperties
+import mikufan.cx.yc.core.ical.util.debugName
 import net.fortuna.ical4j.model.component.VEvent
 
 /**
@@ -11,22 +13,30 @@ import net.fortuna.ical4j.model.component.VEvent
  */
 class EventMapper(
   private val dateTimeFieldSetter: DateTimeFieldSetter,
+  private val alarmMapper: AlarmMapper,
 ) {
 
   fun doMap(toBeMapped: ToBeMappedYouTrackIssueInfo): VEvent {
     val json = toBeMapped.json
     val dateTimeFieldInfo = toBeMapped.dateTimeFieldInfo
+    log.info { "Start mapping ${json.debugName} to VEvent" }
     val vEvent = VEvent()
     dateTimeFieldSetter.doMapAndSet(vEvent, json, dateTimeFieldInfo)
     vEvent.addCommonPropertiesFrom(json)
-
+    val alarmSetting = toBeMapped.alarmSetting
+    alarmMapper.createAlarm(json, alarmSetting)?.let {
+      vEvent.add(it)
+    }
+    log.info { "Done mapping ${json.debugName} to VEvent" }
     return vEvent
   }
 
   private fun VEvent.addCommonPropertiesFrom(json: YouTrackIssueJson) {
     val idReadable = json["idReadable"].asText()
-    val summary = "$idReadable - ${json["summary"].asText()}"
+    val summary = "[$idReadable] ${json["summary"].asText()}"
     val description = json["description"].asText()
     addCommonProperties(idReadable, summary, description)
   }
 }
+
+private val log = KInlineLogging.logger()
---
+++ yc-core-ical/src/test/kotlin/mikufan/cx/yc/core/ical/component/EventMapperIT.kt
@@ -0,0 +1,58 @@
+package mikufan.cx.yc.core.ical.component
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.kotest.assertions.failure
+import io.kotest.core.spec.style.ShouldSpec
+import mikufan.cx.yc.core.ical.model.AlarmSetting
+import mikufan.cx.yc.core.ical.model.OneDayIssueDateTimeFieldInfo
+import mikufan.cx.yc.core.ical.model.OneDayIssueInfo
+import mikufan.cx.yc.core.ical.model.OtherStringMappings
+import mikufan.cx.yc.core.ical.model.exception.MappingException
+import mikufan.cx.yc.core.ical.util.YouTrackIssueJson
+import mikufan.cx.yc.core.ical.util.loadResourceAsString
+import java.time.Duration
+import java.time.ZoneId
+
+class EventMapperIT : ShouldSpec({
+
+  val eventMapper = EventMapper(DateTimeFieldSetter(), AlarmMapper())
+  context("mapping of the YouTrack Issue Json to a whole day event without alarm") {
+    val issues = ObjectMapper().readTree(loadResourceAsString("sample-json/with-prio-state-assignee-due-rsp.json"))
+    val dateFieldInfo = OneDayIssueDateTimeFieldInfo("Due Date", ZoneId.of("Canada/Eastern"))
+    issues.forEach { json ->
+      should("works on ${json["idReadable"].asText()}") {
+        val toBeMapped =
+          OneDayIssueInfo(json as YouTrackIssueJson, dateFieldInfo, null, OtherStringMappings(emptyList()))
+        try {
+          val vEvent = eventMapper.doMap(toBeMapped)
+          println(vEvent)
+        } catch (e: MappingException) {
+          e.printStackTrace()
+        } catch (e: Exception) {
+          e.printStackTrace()
+          failure("unexpected exception", e)
+        }
+      }
+    }
+  }
+  context("mapping of YouTrack Issue Json to a whole day event with alarm") {
+    val issues = ObjectMapper().readTree(loadResourceAsString("sample-json/with-all-custom-fields-rsp-2023.json"))
+    val dateFieldInfo = OneDayIssueDateTimeFieldInfo("Due Date", ZoneId.of("Canada/Eastern"))
+    issues.forEach { json ->
+      should("works on ${json["idReadable"].asText()}") {
+        val alarmSetting = AlarmSetting("Estimation", true, Duration.ofHours(2))
+        val toBeMapped =
+          OneDayIssueInfo(json as YouTrackIssueJson, dateFieldInfo, alarmSetting, OtherStringMappings(emptyList()))
+        try {
+          val vEvent = eventMapper.doMap(toBeMapped)
+          println(vEvent)
+        } catch (e: MappingException) {
+          e.printStackTrace()
+        } catch (e: Exception) {
+          e.printStackTrace()
+          failure("unexpected exception", e)
+        }
+      }
+    }
+  }
+})
