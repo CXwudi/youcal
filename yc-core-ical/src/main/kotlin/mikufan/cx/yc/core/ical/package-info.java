d336ba87d347a19dae5967f79c920f2ec4dc0df2
CXwudi <cx2010017@gmail.com>
2023-01-04 17:04:46 -0500

    better info

--- yc-core-ical/src/main/kotlin/mikufan/cx/yc/core/ical/package-info.java
+++ yc-core-ical/src/main/kotlin/mikufan/cx/yc/core/ical/package-info.java
@@ -1,9 +1,11 @@
 /**
  * Contains any lib about creating the iCal component from an issue component in json form from YouTrack.
  * <p>
- * Can be either list of issues to a completed iCal file, or a single issue (component) to a single iCal component.
+ * The main class is {@link mikufan.cx.yc.core.ical.component.EventMapper} which take a
+ * {@link mikufan.cx.yc.core.ical.model.ToBeMappedYouTrackIssueInfo} as the input and return
+ * the {@link net.fortuna.ical4j.model.component.VEvent} as the output.
  * <p>
- * 2022-10-10
+ * 2023-01-04
  *
  * @author CX无敌
  */
