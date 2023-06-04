429d29f42e9004b5358ae6403d2a03521a5055eb
CXwudi <cx2010017@gmail.com>
2023-06-03 21:37:52 -0400

    remove unneeded gradle config

--- gradle/plugins/mixin/spring-boot-kotlin/src/main/kotlin/my/mixin/spring-boot-kotlin.gradle.kts
+++ gradle/plugins/mixin/spring-boot-kotlin/src/main/kotlin/my/mixin/spring-boot-kotlin.gradle.kts
@@ -1,8 +1,5 @@
 package my.mixin
 
-import my.mixin.springboot.kotlin.MockFramework
-import my.mixin.springboot.kotlin.MySpringBootKotlinExtension
-
 /**
  * require kotlin-mixin and spring-boot-mixin
  */
@@ -11,35 +8,18 @@ plugins {
   kotlin("plugin.spring")
 }
 
+configurations {
+  all {
+    exclude(group = "org.mockito")
+    exclude(group = "org.mockito.kotlin")
+  }
+}
+
 dependencies {
   implementation("com.github.CXwudi:kotlin-jvm-inline-logging")
   testImplementation("io.kotest.extensions:kotest-extensions-spring")
+  testImplementation("io.mockk:mockk")
+  testImplementation("com.ninja-squad:springmockk")
   // due to https://github.com/Ninja-Squad/springmockk#limitations, mockk and springmockk are not added by default here,
   // use my.spring-boot-mockk-mixin to add mockk instead
-}
-
-extensions.create<MySpringBootKotlinExtension>("mySpringBootKotlin")
-
-afterEvaluate {
-  val ext = this.extensions.getByType<MySpringBootKotlinExtension>()
-  when (ext.mockFramework.get()) {
-    MockFramework.MOCKITO -> {
-      this.dependencies {
-        testImplementation("org.mockito.kotlin:mockito-kotlin")
-      }
-    }
-
-    MockFramework.MOCKK -> {
-      this.configurations {
-        all {
-          exclude(group = "org.mockito")
-          exclude(group = "org.mockito.kotlin")
-        }
-      }
-      this.dependencies {
-        testImplementation("io.mockk:mockk")
-        testImplementation("com.ninja-squad:springmockk")
-      }
-    }
-  }
 }
